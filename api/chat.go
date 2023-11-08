42bfc3c6a186f4245f2f70d0ed3fd5d0235d3cfb
linziwei2 <915504797g@gmail.com>
2023-11-08 20:29:07 +0800

    ping

--- api/chat.go
+++ api/chat.go
@@ -15,19 +15,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// 当前端请求api时，返回{code: 200, data: 'pong'}内容给他
-func Ping(w http.ResponseWriter, r *http.Request) {
-	// 返回HTTP状态码200
-	w.WriteHeader(http.StatusOK)
-	// 返回http响应内容
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	w.Header().Set("Access-Control-Max-Age", "86400")
-	// 返回内容
-	fmt.Fprintf(w, "{code: 200, data: 'pong123456789'}")
-}
-
 type JsonData struct {
 	Messages    interface{} `json:"messages"`
 	Model       string      `json:"model"`
---
+++ api/ping.go
@@ -0,0 +1,19 @@
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 当前端请求api时，返回{code: 200, data: 'pong'}内容给他
+func ping(w http.ResponseWriter, r *http.Request) {
+	// 返回HTTP状态码200
+	w.WriteHeader(http.StatusOK)
+	// 返回http响应内容
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Max-Age", "86400")
+	// 返回内容
+	fmt.Fprintf(w, "{code: 200, data: 'pong123456789'}")
+}
