cubatic45/dotfiles

commit e064ffd0e9d713ba9aa994cc569822a04ac8648d

fix: null prefix and suffix

main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/gin-gonic/gin"
 
 	"bufio"
@@ -45,7 +47,8 @@ type JsonData struct {
 }
 
 type Delta struct {
-	Content string `json:"content"`
+	Role    string `json:"role,omitempty"`
+	Content string `json:"content,omitempty"`
 }
 
 type Choice struct {
@@ -57,6 +60,8 @@ type Data struct {
 	Choices []Choice `json:"choices,omitempty"`
 	Created int      `json:"created,omitempty"`
 	ID      string   `json:"id,omitempty"`
+	Object  string   `json:"object,omitempty"`
+	Model   string   `json:"model,omitempty"`
 }
 
 // Generate a random hexadecimal string of the specified length.
@@ -169,13 +174,30 @@ func chatCompletions(c *gin.Context) {
 					object = "chat.completion"
 				}
 
-				if bytes.Contains(line, []byte(`"choices":`)) {
-					if !bytes.Contains(line, []byte(`"object":`)) {
-						line = bytes.ReplaceAll(line, []byte(`"choices":`), []byte(fmt.Sprintf(`"object": "%s", "choices":`, object)))
+				if len(line) > 0 && !bytes.Contains(line, []byte("data: [Done]")) {
+					tmp := strings.TrimPrefix(string(line), "data: ")
+					data := &Data{}
+					if err := json.Unmarshal([]byte(tmp), &data); err != nil {
+						fmt.Println(err)
 					}
-					if !bytes.Contains(line, []byte(`"model":`)) {
-						line = bytes.ReplaceAll(line, []byte(`"choices":`), []byte(fmt.Sprintf(`"model": "%s", "choices":`, jsonBody.Model)))
+					if len(data.Choices) == 0 {
+						continue
 					}
+					if data.Object == "" {
+						data.Object = object
+					}
+					if data.Model == "" {
+						data.Model = jsonBody.Model
+					}
+					if data.Created == 0 {
+						data.Created = int(time.Now().Unix())
+					}
+
+					newLine, err := json.Marshal(data)
+					if err != nil {
+						fmt.Println(err)
+					}
+					line = []byte(fmt.Sprintf("data: %s", string(newLine)))
 				}
 
 				c.Writer.Write(line)

commit 36ab54e5cebc9b6c1a6e56399f527521eecb0a2a

fix: mistake when stream is fale
It's a bug introduced in e064ffd

Fix #162

main.go
@@ -51,9 +51,15 @@ type Delta struct {
 	Content string `json:"content,omitempty"`
 }
 
+type Message struct {
+	Role    string `json:"role,omitempty"`
+	Content string `json:"content,omitempty"`
+}
+
 type Choice struct {
-	Delta Delta `json:"delta"`
-	Index int   `json:"index"`
+	Delta   Delta   `json:"delta,omitempty"`
+	Message Message `json:"message,omitempty"`
+	Index   int     `json:"index"`
 }
 
 type Data struct {
@@ -197,7 +203,11 @@ func chatCompletions(c *gin.Context) {
 					if err != nil {
 						fmt.Println(err)
 					}
-					line = []byte(fmt.Sprintf("data: %s", string(newLine)))
+					if jsonBody.Stream {
+						line = []byte(fmt.Sprintf("data: %s", string(newLine)))
+					} else {
+						line = newLine
+					}
 				}
 
 				c.Writer.Write(line)